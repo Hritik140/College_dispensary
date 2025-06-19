317aeb5c6e8427e3db14c9ede222486354ff4ca8
Mashhood Ahmad Danish <46836316+mashho@users.noreply.github.com>
2025-06-19 17:15:52 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://mashhooddanish:otfRG4r52l2IxOfN@cluster0.65lsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+const mongoDBURL = "" ; // Add your database URL
+mongoose.connect(mongoDBURL)
     .then(res=>{
         console.log("DataBase Connected Successfully")
 }).catch(err=>{
